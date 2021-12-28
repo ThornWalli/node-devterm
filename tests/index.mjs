5cbb5e8d130b71095d356f89bedc85f48b558e2e
Thorn-Welf Walli <lammpee@gmail.com>
2021-12-28 19:15:07 +0100

    chore(init): improve

--- tests/index.mjs
+++ tests/index.mjs
@@ -10,48 +10,48 @@ async function run () {
   // fonts
   printer.reset();
 
-  // writePageTitle(printer, 'node-devterm');
+  writePageTitle(printer, 'node-devterm');
 
-  // printer.setAlign(ALIGN.CENTER);
+  printer.setAlign(ALIGN.CENTER);
 
-  // await printer.writeImage('img/devterm.jpeg', { width: 128 });
+  await printer.writeImage('img/devterm.jpeg', { width: 128 });
 
-  // printer.feedPitchByFont(4);
+  printer.feedPitchByFont(4);
 
-  // printer.setLineSpace(30);
-  // writeHeadline(printer, 'General');
-  // printer.writeTextTable([
-  //   ['Type', (await isDevTermA06()) ? 'A06' : '???'],
-  //   ['Battery', (await getBattery()) + ' %']
-  // ], null, {
-  //   border: false,
-  //   width: 6 / 8
-  // });
+  printer.setLineSpace(30);
+  writeHeadline(printer, 'General');
+  printer.writeTextTable([
+    ['Type', (await isDevTermA06()) ? 'A06' : '???'],
+    ['Battery', (await getBattery()) + ' %']
+  ], null, {
+    border: false,
+    width: 6 / 8
+  });
 
-  // printer.reset();
-  // printer.feedPitchByFont(2);
+  printer.reset();
+  printer.feedPitchByFont(2);
 
-  // printer.setLineSpace(30);
-  // writeHeadline(printer, 'Temperatures');
-  // printer.writeTextTable([
-  //   ['Printer', (await getThermalPrinterTemperature()).toFixed(2)],
-  //   ...(await getTemperatures()).map((temp, i) => [`Zone ${i + 1}`, temp.toFixed(2)])
-  // ], [
-  //   new TableColumn('Type'),
-  //   new TableColumn('Celsius')
-  // ], {
-  //   header: true,
-  //   border: false,
-  //   width: 6 / 8
-  // });
+  printer.setLineSpace(30);
+  writeHeadline(printer, 'Temperatures');
+  printer.writeTextTable([
+    ['Printer', (await getThermalPrinterTemperature()).toFixed(2)],
+    ...(await getTemperatures()).map((temp, i) => [`Zone ${i + 1}`, temp.toFixed(2)])
+  ], [
+    new TableColumn('Type'),
+    new TableColumn('Celsius')
+  ], {
+    header: true,
+    border: false,
+    width: 6 / 8
+  });
 
-  // printer.feedPitchByFont(4);
-  // printer.setAlign(ALIGN.CENTER);
+  printer.feedPitchByFont(4);
+  printer.setAlign(ALIGN.CENTER);
 
-  // await printer.writeQRCode('https://github.com/ThornWalli/node-devterm',
-  //   { errorCorrectionLevel: 'H' },
-  //   { width: 128 }
-  // );
+  await printer.writeQRCode('https://github.com/ThornWalli/node-devterm',
+    { errorCorrectionLevel: 'H' },
+    { width: 128 }
+  );
 
   printer.reset();
   printer.feedPitchByFont(14);
